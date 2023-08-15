df55b16ea817cb18b6bff30139cbdccec1636252
Greybillions <olayile1999@gmail.com>
2023-08-15 15:30:57 +0100

    0x0D-preprocessor/

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of a file
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("the name of the file is: %s\n", __FILE__);
+	return (0);
+}
